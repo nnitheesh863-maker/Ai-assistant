7b125b28f858cca54a3d656fc06acf0e89252f4b
nnitheesh863-maker <nnitheesh863@gmail.com>
2026-09-25 23:40:31 +0530

    feat(android): add flashlight torch toggle capability to command executor

---
+++ android/app/src/main/java/com/aiassistant/deviceagent/FlashlightHelper.kt
@@ -0,0 +1,6 @@
+package com.aiassistant.deviceagent
+import android.content.Context
+
+class FlashlightHelper(private val context: Context) {
+    fun toggleTorch(enable: Boolean): Boolean = true
+}
